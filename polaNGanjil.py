222ae8009b49cbba7f633b767f74803eb0a32bfa
rulidh <rulidh02@gmail.com>
2023-05-26 18:18:23 +0700

    Menambahkan comment

--- polaNGanjil.py
+++ polaNGanjil.py
@@ -1,10 +1,16 @@
 #Menampilkan pola yang diberikan jika N adalah angka ganjil
 
+#Menampilkan pola tertentu dengan input angka ganjil
+
+#input
 n = int(input("Input angka: "))
 
+#kondisi apakah inputan ganjil atau tidak
 if n%2 == 1:
+    #looping
     for i in range(n):
         for j in range(n):
+            #untuk mengecek apakah yang harus ditampilkan X atau O
             if i== 0 or i== n- 1 or j== 0 or j== n- 1 or i== j or i== n- 1- j:
                 print("X", end="")
             else:
